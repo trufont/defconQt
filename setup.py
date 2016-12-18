4220f28fa1a303570e2c52f6f5a16fba658cd13b
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-12-18 13:46:02 +0000

    setup.py: add install_requires and metadata for PyPI, plus minor fixes

--- setup.py
+++ setup.py
@@ -1,44 +1,41 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
-try:
-    import fontTools  # noqa
-except:
-    print("*** Warning: defconQt requires fontTools, see:")
-    print("    https://github.com/behdad/fonttools")
 
-try:
-    import ufoLib  # noqa
-except:
-    print("*** Warning: defconQt requires ufoLib, see:")
-    print("    https://github.com/unified-font-object/ufoLib")
-
-try:
-    import defcon  # noqa
-except:
-    print("*** Warning: defconQt requires defcon, see:")
-    print("    https://github.com/typesupply/defcon")
+# TODO: add README.rst, so we can use on both Github and PyPI
+# with open('README.rst', 'r') as f:
+#     long_description = f.read()
 
 setup(
     name="defconQt",
     version="0.2.0.dev0",
     description="A set of Qt objects for use in defcon applications.",
+    # long_description=long_description,
     author="Adrien Tétar",
     author_email="adri-from-59@hotmail.fr",
-    url="http://adrientetar.github.io",
-    packages=[
-        "defconQt",
-        "defconQt.controls",
-        "defconQt.representationFactories",
-        "defconQt.tools",
-        "defconQt.windows",
-    ],
+    url="https://github.com/trufont/defconQt",
+    license="GNU LGPL v3/GNU GPL v3",
     package_dir={"": "Lib"},
+    packages=find_packages("Lib"),
     platforms=["Win32", "Mac OS X", "Linux"],
+    install_requires=[
+        "pyqt5>=5.5.0",
+        "fonttools>=3.3.1",
+        "ufoLib>=2.0.0",
+        "defcon>=0.2.0",
+    ],
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Environment :: GUI",
-        "Programming Language :: Python :: 3.4",
+        "Environment :: X11 Applications :: Qt",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
         "Intended Audience :: Developers",
         "Topic :: Text Processing :: Fonts",
+        'Topic :: Multimedia :: Graphics :: Editors :: Vector-Based',
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
     ],
 )
